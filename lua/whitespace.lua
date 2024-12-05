4b2c26ed107187015ab4ff2d272c01df19ce1156
aidancz <aidanczhai@gmail.com>
2024-12-05 15:31:14 +0800

    big update

--- lua/whitespace.lua
+++ lua/whitespace.lua
@@ -29,65 +29,84 @@ end
 -- # config
 
 M.config = {
-	only_in_normal_buffers = true,
-	set_init_switches = function()
-		vim.b.whitespace_switch_1 = false
-		vim.b.whitespace_switch_2 = false
-		vim.b.whitespace_switch_3 = false
-		vim.b.whitespace_switch_4 = true
-	end,
+	excluded_filetypes = {},
+	excluded_buftypes = {".+"},
+	init_switches = {
+		false,
+		false,
+		false,
+		true,
+	},
 }
 -- whitespace 1: space
 -- whitespace 2: multispace
 -- whitespace 3: tab
 -- whitespace 4: trail
 
--- # set_init_status
+-- # set_status
 
-M.set_init_status = function()
-	M.config.set_init_switches()
+M.set_status0 = function()
 
-	vim.b.whitespace_id_1 = nil
-	vim.b.whitespace_id_2 = nil
-	vim.b.whitespace_id_3 = nil
-	vim.b.whitespace_id_4 = nil
+	vim.w.whitespace_switch_1 = nil
+	vim.w.whitespace_switch_2 = nil
+	vim.w.whitespace_switch_3 = nil
+	vim.w.whitespace_switch_4 = nil
 
-	if vim.api.nvim_get_mode().mode == 'i' then
-		vim.b.whitespace_insert = true
-	else
-		vim.b.whitespace_insert = false
-	end
+	vim.w.whitespace_insert = nil
+
+end
+
+M.set_status1 = function()
+
+	vim.w.whitespace_switch_1 = M.config.init_switches[1]
+	vim.w.whitespace_switch_2 = M.config.init_switches[2]
+	vim.w.whitespace_switch_3 = M.config.init_switches[3]
+	vim.w.whitespace_switch_4 = M.config.init_switches[4]
+
+	vim.w.whitespace_insert = (vim.api.nvim_get_mode().mode == 'i')
+
+end
+
+M.set_status2 = function()
+
+        if vim.w.whitespace_switch_1 == nil then vim.w.whitespace_switch_1 = M.config.init_switches[1] end
+        if vim.w.whitespace_switch_2 == nil then vim.w.whitespace_switch_2 = M.config.init_switches[2] end
+        if vim.w.whitespace_switch_3 == nil then vim.w.whitespace_switch_3 = M.config.init_switches[3] end
+        if vim.w.whitespace_switch_4 == nil then vim.w.whitespace_switch_4 = M.config.init_switches[4] end
+
+        if vim.w.whitespace_insert == nil then vim.w.whitespace_insert = (vim.api.nvim_get_mode().mode == 'i') end
+
 end
 
 -- # match
 
 M.match_add = function()
-	if vim.b.whitespace_switch_1 then
-		vim.b.whitespace_id_1 = vim.fn.matchadd('Whitespace1', [[ ]], 1)
+	if vim.w.whitespace_switch_1 then
+		vim.w.whitespace_id_1 = vim.fn.matchadd('Whitespace1', [[ ]], 1)
 	end
 
-	if vim.b.whitespace_switch_2 then
-		vim.b.whitespace_id_2 = vim.fn.matchadd('Whitespace2', [[ \{2,}]], 2)
+	if vim.w.whitespace_switch_2 then
+		vim.w.whitespace_id_2 = vim.fn.matchadd('Whitespace2', [[ \{2,}]], 2)
 	end
 
-	if vim.b.whitespace_switch_3 then
-		vim.b.whitespace_id_3 = vim.fn.matchadd('Whitespace3', [[\t]], 3)
+	if vim.w.whitespace_switch_3 then
+		vim.w.whitespace_id_3 = vim.fn.matchadd('Whitespace3', [[\t]], 3)
 	end
 
-	if vim.b.whitespace_switch_4 then
-		if vim.b.whitespace_insert then
-			vim.b.whitespace_id_4 = vim.fn.matchadd('Whitespace4', [[\s\+\%#\@<!$]], 4)
+	if vim.w.whitespace_switch_4 then
+		if vim.w.whitespace_insert then
+			vim.w.whitespace_id_4 = vim.fn.matchadd('Whitespace4', [[\s\+\%#\@<!$]], 4)
 		else
-			vim.b.whitespace_id_4 = vim.fn.matchadd('Whitespace4', [[\s\+$]], 4)
+			vim.w.whitespace_id_4 = vim.fn.matchadd('Whitespace4', [[\s\+$]], 4)
 		end
 	end
 end
 
 M.match_del = function()
-	pcall(vim.fn.matchdelete, vim.b.whitespace_id_1)
-	pcall(vim.fn.matchdelete, vim.b.whitespace_id_2)
-	pcall(vim.fn.matchdelete, vim.b.whitespace_id_3)
-	pcall(vim.fn.matchdelete, vim.b.whitespace_id_4)
+	pcall(vim.fn.matchdelete, vim.w.whitespace_id_1)
+	pcall(vim.fn.matchdelete, vim.w.whitespace_id_2)
+	pcall(vim.fn.matchdelete, vim.w.whitespace_id_3)
+	pcall(vim.fn.matchdelete, vim.w.whitespace_id_4)
 	-- use `pcall` because there is an error if match id is not present
 end
 
@@ -98,61 +117,80 @@ end
 
 -- # autocmd
 
-H.buffer_normal_p = function()
-	if vim.bo[0].buftype == '' then
-		return true
-	else
-		return false
+H.excluded_filetype_p = function()
+	for _, pattern in pairs(M.config.excluded_filetypes) do
+		if string.find(vim.bo.filetype, pattern) then
+			return true
+		end
 	end
+	return false
+end
+
+H.excluded_buftype_p = function()
+	for _, pattern in pairs(M.config.excluded_buftypes) do
+		if string.find(vim.bo.buftype, pattern) then
+			return true
+		end
+	end
+	return false
 end
 
 M.create_autocmd = function()
-	local whitespace_augroup = vim.api.nvim_create_augroup('whitespace', {clear = true})
-
 	vim.api.nvim_create_autocmd(
 		{
-			'WinEnter',
 			'BufEnter',
+			'WinEnter',
 		},
 		{
-			group = whitespace_augroup,
-			callback = function()
-				local a = M.config.only_in_normal_buffers
-				local b = H.buffer_normal_p()
-				if a and (not b) then
+			group = vim.api.nvim_create_augroup('whitespace0', {clear = true}),
+			callback = function(arg)
+				vim.schedule(function()
+				-- use `vim.schedule` because:
+				-- https://github.com/neovim/neovim/issues/29419
+				----------------------------------------------------------------
+				if H.excluded_filetype_p() or H.excluded_buftype_p() then
+					M.set_status0()
+					M.match_sync()
 					return
 				end
 
+				if arg.event == "BufEnter" then
+					M.set_status1()
+					M.match_sync()
+				end
 
+				if arg.event == "WinEnter" then
+					M.set_status2()
+					M.match_sync()
+				end
 
-				M.set_init_status()
-				M.match_sync()
-
-
-
+				local whitespace1_augroup = vim.api.nvim_create_augroup('whitespace1', {clear = true})
 				vim.api.nvim_create_autocmd(
 					{
 						'InsertEnter',
 					},
 					{
-						group = whitespace_augroup,
+						group = whitespace1_augroup,
+						buffer = arg.buf,
 						callback = function()
-							vim.b.whitespace_insert = true
+							vim.w.whitespace_insert = true
 							M.match_sync()
 						end,
 					})
-
 				vim.api.nvim_create_autocmd(
 					{
 						'InsertLeave',
 					},
 					{
-						group = whitespace_augroup,
+						group = whitespace1_augroup,
+						buffer = arg.buf,
 						callback = function()
-							vim.b.whitespace_insert = false
+							vim.w.whitespace_insert = false
 							M.match_sync()
 						end,
 					})
+				----------------------------------------------------------------
+				end)
 			end,
 		})
 end
@@ -167,4 +205,5 @@ M.create_default_hl = function()
 end
 
 -- # return
+Whitespace = M
 return M
